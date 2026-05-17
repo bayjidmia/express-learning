6e332a0dbd5e70f58e25a1895c465152d639587c
bayjidmia <mdbayjidmia982@gmail.com>
2026-05-17 14:15:01 +0600

    first save user in neodb using table and post method

--- src/server.ts
+++ src/server.ts
@@ -18,8 +18,8 @@ const initdb = async () => {
     await pool.query(`CREATE TABLE IF NOT EXISTS users (
       id SERIAL PRIMARY KEY,
       name VARCHAR(20) NOT NULL,
-      email VARCHAR(50) NOT NULL UNIQUE,
-      password VARCHAR(255) NOT NULL,
+      email VARCHAR(20) NOT NULL UNIQUE,
+      password VARCHAR(20) NOT NULL,
       is_active BOOLEAN DEFAULT true,
       age int,
       created_at TIMESTAMP DEFAULT Now(),
@@ -40,15 +40,24 @@ app.get("/", (req: Request, res: Response) => {
   });
 });
 
-app.post("/", async (req: Request, res: Response) => {
-  const { name, email, password } = req.body;
-  res.status(201).json({
-    message: "created",
-    data: {
-      name,
-      email,
-    },
-  });
+app.post("/api/users", async (req: Request, res: Response) => {
+  const { name, email, password, age } = req.body;
+  try {
+    const result = await pool.query(
+      `INSERT INTO users (name, email, password, age) VALUES ($1, $2, $3, $4) RETURNING *`,
+      [name, email, password, age],
+    );
+
+    res.status(201).json({
+      message: " user created sucessfully",
+      data: result.rows[0],
+    });
+  } catch (error: any) {
+    res.status(500).json({
+      message: error.message,
+      error: error,
+    });
+  }
 });
 
 app.listen(port, () => {
